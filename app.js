2ce4e91989c82d546fb0a87003b38f8f15e8fd73
andrewboylan <andreweboylan@gmail.com>
2023-11-10 18:43:18 -0600

    finished I guess

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-import express from 'express'
-import Hello from './hello.js'
-const app = express()
-Hello(app)
-app.listen(4000)
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from './Lab5.js';
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js'
+import AssignmentRoutes from './assignments/routes.js'
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+        credientials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+
+ModuleRoutes(app)
+CourseRoutes(app);
+AssignmentRoutes(app);
+
+Lab5(app);
+
+Hello(app);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
---
+++ assignments/routes.js
@@ -0,0 +1,52 @@
+import Database from "../Database/index.js";
+
+function AssignmentRoutes(app) {
+    app.get("/api/assignments", (req, res) => {
+        const assignments = Database.assignments;
+        res.send(assignments);
+    });
+
+    app.get("/api/assignments/:id", (req, res) => {
+        const { id } = req.params;
+        const assignment = Database.assignments.find((a) => a._id === id);
+        if (!assignment) {
+            res.status(404).send("Assignment not found");
+            return;
+        }
+        res.send(assignment);
+    });
+
+    // Create assignment
+    app.post("/api/assignments", (req, res) => {
+        const assignment = { ...req.body, _id: new Date().getTime().toString() };
+        Database.assignments.push(assignment);
+        res.send(assignment);
+    });
+
+    app.delete("/api/assignments/:id", (req, res) => {
+        const { id } = req.params;
+        Database.assignments = Database.assignments.filter((a) => a._id !== id);
+        res.sendStatus(204);
+    });
+
+    app.put("/api/assignments/:id", (req, res) => {
+        const { id } = req.params;
+        const updatedAssignment = req.body;
+        let found = false;
+        Database.assignments = Database.assignments.map((a) => {
+            if (a._id === id) {
+                found = true;
+                return { ...a, ...updatedAssignment };
+            }
+            return a;
+        });
+
+        if (found) {
+            res.sendStatus(204);
+        } else {
+            res.status(404).send("Assignment not found");
+        }
+    });
+}
+
+export default AssignmentRoutes;
